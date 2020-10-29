b0052755dcaa2e76aca5382dd8839b81fb8c122a
Lubebul <davide.lubo@gmail.com>
2020-10-29 20:22:29 +0100

    modifica

--- TAP1718_Laboratorio3_Git/Program.cs
+++ TAP1718_Laboratorio3_Git/Program.cs
@@ -3,10 +3,22 @@
 namespace TAP1718_Laboratorio3_Git
 {
     class Program
-    {
+    {   
         public static void Main(string[] args)
         {
             Console.WriteLine("Se leggete questo messaggio, significa che siete tornati indietro nel tempo!");
+            Program.Specchio();
         }
+        
+        private static void Specchio()
+        {
+            Console.WriteLine("Inserisci una frase ed io la renderò in CAPS (se possibile)\n");
+            string aux = Console.ReadLine();
+            Console.WriteLine(aux.ToUpper());
+            Console.ReadLine();
+        }
+
+
+
     }
 }
